1d83a9fd5caeb82fb897bb96fb0f85d7a030618b
sesp13 <sespinosarte99@gmail.com>
2022-04-04 11:09:38 -0500

    01-cliTest: add new stuff created with the angular cli

--- 01-cli-test/src/app/app.module.ts
+++ 01-cli-test/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule
