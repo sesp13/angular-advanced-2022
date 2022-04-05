9b02b2c8551e5fd1875d4da364a08ad071bec7c7
sesp13 <sespinosarte99@gmail.com>
2022-04-05 15:03:01 -0500

    03-adminPro: add more interaction to the progress bars

--- 03-adminPro/src/app/components/incrementer/incrementer.component.ts
+++ 03-adminPro/src/app/components/incrementer/incrementer.component.ts
@@ -1,21 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-incrementer',
   templateUrl: './incrementer.component.html',
   styleUrls: ['./incrementer.component.scss'],
 })
-export class IncrementerComponent {
-  progress: number = 50;
+export class IncrementerComponent implements OnInit {
+  @Input('value') progress: number = 50;
+  @Input() btnClass: string = 'btn-primary';
+
+  @Output() onValueChange: EventEmitter<number> = new EventEmitter<number>();
+
+  ngOnInit(): void {
+    this.btnClass = `btn ${this.btnClass}`;
+  }
+
   changeValue(value: number): void {
     if (this.progress + value >= 100) {
       this.progress = 100;
+      this.onValueChange.emit(this.progress);
       return;
     }
     if (this.progress + value <= 0) {
       this.progress = 0;
+      this.onValueChange.emit(this.progress);
       return;
     }
     this.progress += value;
+    this.onValueChange.emit(this.progress);
+  }
+
+  onChange(value: number) {
+    if (value >= 100) {
+      this.progress = 100;
+    } else if (value <= 0) {
+      this.progress = 0;
+    } else {
+      this.progress = value;
+    }
+    this.onValueChange.emit(value);
   }
 }
--- 03-adminPro/src/app/pages/progress/progress.component.ts
+++ 03-adminPro/src/app/pages/progress/progress.component.ts
@@ -6,9 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./progress.component.scss'],
 })
 export class ProgressComponent {
-  progress: number = 50;
+  progress1: number = 25;
+  progress2: number = 35;
 
-  get getPercentage(): string {
-    return `${this.progress}%`;
+  get getPercentage1(): string {
+    return `${this.progress1}%`;
+  }
+
+  get getPercentage2(): string {
+    return `${this.progress2}%`;
+  }
+
+  progressChange(value: number, type: string) {
+    if (type == '1') {
+      this.progress1 = value;
+    } else if (type == '2') {
+      this.progress2 = value;
+    }
   }
 }
