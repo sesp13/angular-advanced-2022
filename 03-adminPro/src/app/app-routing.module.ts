b99030462802125f3d2d263d742371bf7c32d445
sesp13 <sespinosarte99@gmail.com>
2022-04-04 19:15:04 -0500

    03-adminPro: use indepent router modules

--- 03-adminPro/src/app/app-routing.module.ts
+++ 03-adminPro/src/app/app-routing.module.ts
@@ -1,46 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from './auth/register/register.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { Graph1Component } from './pages/graph1/graph1.component';
+import { AuthRoutingModule } from './auth/auth-routing.module';
 import { NotFoundComponent } from './not-found/not-found.component';
-import { PagesComponent } from './pages/pages.component';
-import { ProgressComponent } from './pages/progress/progress.component';
+import { PagesRoutingModule } from './pages/pages.routing.module';
 
 const routes: Routes = [
-  {
-    path: '',
-    component: PagesComponent,
-    children: [
-      // array of protected routes
-      {
-        path: 'dashboard',
-        component: DashboardComponent,
-      },
-      {
-        path: 'progress',
-        component: ProgressComponent,
-      },
-      {
-        path: 'graph1',
-        component: Graph1Component,
-      },
-      {
-        path: '',
-        redirectTo: 'dashboard',
-        pathMatch: 'full',
-      },
-    ],
-  },
-  {
-    path: 'login',
-    component: LoginComponent,
-  },
-  {
-    path: 'register',
-    component: RegisterComponent,
-  },
+  // Path /dashboard PagesRouting
+  // Path /auth AuthRouting
   {
     path: '**',
     component: NotFoundComponent,
@@ -48,7 +14,12 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes),
+    // Direct import without lazy load
+    PagesRoutingModule,
+    AuthRoutingModule,
+  ],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
