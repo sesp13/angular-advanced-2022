9797618ece8ab413725221fa4c6e79da51d08f02
sesp13 <sespinosarte99@gmail.com>
2022-04-25 15:43:33 -0500

    03-adminPro: add lazyloading

--- 03-adminPro/src/app/guards/auth.guard.ts
+++ 03-adminPro/src/app/guards/auth.guard.ts
@@ -2,8 +2,12 @@ import { Injectable } from '@angular/core';
 import {
   ActivatedRouteSnapshot,
   CanActivate,
+  CanLoad,
+  Route,
   Router,
   RouterStateSnapshot,
+  UrlSegment,
+  UrlTree,
 } from '@angular/router';
 import { Observable, tap } from 'rxjs';
 import { UserService } from '../services/user.service';
@@ -11,13 +15,23 @@ import { UserService } from '../services/user.service';
 @Injectable({
   providedIn: 'root',
 })
-export class AuthGuard implements CanActivate {
+export class AuthGuard implements CanActivate, CanLoad {
   constructor(private userService: UserService, private router: Router) {}
 
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
   ): Observable<boolean> {
+    return this.userService.renewToken().pipe(
+      tap((res) => {
+        if (res === false) {
+          this.router.navigate(['/login']);
+        }
+      })
+    );
+  }
+
+  canLoad(route: Route, segments: UrlSegment[]): Observable<boolean> {
     return this.userService.renewToken().pipe(
       tap((res) => {
         if (res === false) {
--- 03-adminPro/src/app/pages/pages-routing.module.ts
+++ 03-adminPro/src/app/pages/pages-routing.module.ts
@@ -2,119 +2,16 @@ import { Routes, RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 
 import { AuthGuard } from '../guards/auth.guard';
-
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { Graph1Component } from './graph1/graph1.component';
 import { PagesComponent } from './pages.component';
-import { ProgressComponent } from './progress/progress.component';
-import { AccountSettingsComponent } from './account-settings/account-settings.component';
-import { PromisesComponent } from './promises/promises.component';
-import { RxjsComponent } from './rxjs/rxjs.component';
-import { ProfileComponent } from './profile/profile.component';
-
-// Maintenance
-import { DoctorsComponent } from './maintenance/doctors/doctors.component';
-import { DoctorEditComponent } from './maintenance/doctors/doctor-edit/doctor-edit.component';
-import { HospitalsComponent } from './maintenance/hospitals/hospitals.component';
-import { UsersComponent } from './maintenance/users/users.component';
-import { SearchsComponent } from './searchs/searchs.component';
-import { AdminGuard } from '../guards/admin.guard';
 
 const routes: Routes = [
   {
     path: 'dashboard',
     component: PagesComponent,
     canActivate: [AuthGuard],
-    children: [
-      // array of protected routes
-      {
-        path: '',
-        component: DashboardComponent,
-        data: {
-          title: 'Dashboard',
-        },
-      },
-      {
-        path: 'account-settings',
-        component: AccountSettingsComponent,
-        data: {
-          title: 'Account Settings',
-        },
-      },
-      {
-        path: 'graph1',
-        component: Graph1Component,
-        data: {
-          title: 'Graph 1',
-        },
-      },
-      {
-        path: 'profile',
-        component: ProfileComponent,
-        data: {
-          title: 'Profile',
-        },
-      },
-      {
-        path: 'progress',
-        component: ProgressComponent,
-        data: {
-          title: 'Progress Bar',
-        },
-      },
-      {
-        path: 'promises',
-        component: PromisesComponent,
-        data: {
-          title: 'Promises',
-        },
-      },
-      {
-        path: 'rxjs',
-        component: RxjsComponent,
-        data: {
-          title: 'RxJs',
-        },
-      },
-      {
-        path: 'search/:term',
-        component: SearchsComponent,
-        data: {
-          title: 'Application Searchs',
-        },
-      },
-      // Maintenance
-      {
-        path: 'doctors',
-        component: DoctorsComponent,
-        data: {
-          title: 'Doctors',
-        },
-      },
-      {
-        path: 'doctor/:id',
-        component: DoctorEditComponent,
-        data: {
-          title: 'Doctor',
-        },
-      },
-      {
-        path: 'hospitals',
-        component: HospitalsComponent,
-        data: {
-          title: 'Hospitals',
-        },
-      },
-      // Admin route
-      {
-        path: 'users',
-        canActivate: [AdminGuard],
-        component: UsersComponent,
-        data: {
-          title: 'Application users',
-        },
-      },
-    ],
+    canLoad: [AuthGuard],
+    loadChildren: () =>
+      import('./child-routes.module').then((m) => m.ChildRoutesModule),
   },
 ];
 
